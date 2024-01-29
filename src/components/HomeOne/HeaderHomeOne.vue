b1bff52fffefd142a6c9b5df653ca4b4cabcd589
Alexandre Batista <abatista223@gmail.com>
2024-01-29 10:35:01 -0300

    barra superior Style

--- src/components/HomeOne/HeaderHomeOne.vue
+++ src/components/HomeOne/HeaderHomeOne.vue
@@ -12,12 +12,12 @@
               </a>
             </div>
           </div>
-          <div class="col-lg-6 col-md-1 col-sm-1 order-3 order-sm-2">
+          <div class="col-lg-8 col-md-1 col-sm-1 order-3 order-sm-2">
             <div class="appie-header-main-menu">
               <nav-items :menuItems="menuItems" :nasted="nasted" />
             </div>
           </div>
-          <div class="col-lg-4 col-md-7 col-sm-6 col-6 order-2 order-sm-3">
+          <div class="col-lg-2 col-md-7 col-sm-6 col-6 order-2 order-sm-3">
             <div class="appie-btn-box" style="text-align: right">
               <template v-if="this.$store.state.enableRtl">
                 <span
@@ -71,7 +71,7 @@
               <!-- <a class="login-btn" href="#"
                 ><i class="fal fa-user"></i> Login</a
               > -->
-              <a class="main-btn ml-30" href="#">Get Started</a>
+              <a class="main-btn ml-0"  href="#">Get Started</a>
               <div class="toggle-btn ml-30 canvas_open d-lg-none d-block">
                 <i class="fa fa-bars" @click="showSidebar"></i>
               </div>
